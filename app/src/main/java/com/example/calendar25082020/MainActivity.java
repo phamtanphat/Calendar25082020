1845374969349938209eaf7617c79bc058a1e00c
phatdroid94 <phatdroid94@gmail.com>
2020-10-29 21:20:06 +0700

    date picker dialog

--- app/src/main/java/com/example/calendar25082020/MainActivity.java
+++ app/src/main/java/com/example/calendar25082020/MainActivity.java
@@ -2,26 +2,45 @@ package com.example.calendar25082020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    Button mBtnDatePicker;
+    TextView mTvDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Calendar calendar = Calendar.getInstance();
-        int ngay = calendar.get(Calendar.DAY_OF_WEEK);
-        int thang = calendar.get(Calendar.MONTH);
-        int nam = calendar.get(Calendar.YEAR);
-        int gio = calendar.get(Calendar.HOUR_OF_DAY);
-        int phut = calendar.get(Calendar.MINUTE);
-        int giay = calendar.get(Calendar.SECOND);
-        Log.d("BBB",ngay + "/" + (thang + 1) + "/"+ nam);
-//        Log.d("BBB",gio + ":" + phut + ":" + giay);
+
+        mBtnDatePicker = findViewById(R.id.buttonDatePicker);
+        mTvDate = findViewById(R.id.textViewDate);
+
+        mBtnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int yearCurrent = Calendar.getInstance().get(Calendar.YEAR);
+                int monthCurrent = Calendar.getInstance().get(Calendar.MONTH);
+                int dayCurrent = Calendar.getInstance().get(Calendar.DATE);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mTvDate.setText("Ngày lựa chọn : " + dayOfMonth + "/" + (month + 1)+ "/" + year);
+                    }
+                }, yearCurrent, monthCurrent, dayCurrent);
+                datePickerDialog.show();
+
+            }
+        });
+
     }
 }
